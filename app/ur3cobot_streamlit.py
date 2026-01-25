cef88144ad28a01bd1886866786292b2b1fc85a1
Oluwatobi Bello <oluwatobibello887@yahoo.com>
2026-01-25 09:09:43 +0100

    added fix to streamlit app

--- app/ur3cobot_streamlit.py
+++ app/ur3cobot_streamlit.py
@@ -135,7 +135,7 @@ if model and scaler:
                 fig = px.bar(
                     df, 
                     x='Sensor', 
-                    y='Current (A)', 
+                    y='Current', 
                     title="Joint Currents",
                     text_auto='.2f',
                     color='Current',
